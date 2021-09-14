e945ca0f32a948172480f3776962f239c0ff15a9
carycheng <carycheng77@gmail.com>
2021-09-13 19:45:21 -0700

    added controller layer

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,31 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.services';
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessagesService;
+
     @Get()
     listMessages() {
-
+        return this.messagesService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto) {
         console.log(body);
+        return this.messagesService.create(body.content);
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
+    async getMessage(@Param('id') id: string) {
         console.log(id);
+        const message = await this.messagesService.findOne(id);
+
+        if (!message) {
+            throw new NotFoundException('message not found');
+        }
+
+        return message;
     }
 }
